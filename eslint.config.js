7dfcc66324d92848aff235640f43cc0a4cefde38
Jairo Ramirez <jairoramirez_15@hotmail.com>
2025-07-14 18:34:33 -0500

    feature/lint-actions: file .yml for CI/CD and configuration about eslint added

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,46 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import ts from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+
+export default defineConfig([
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  { 
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ignores: ['dist'],
+    plugins: { 
+      'react': pluginReact,
+      'react-hooks': pluginReactHooks
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "react/react-in-jsx-scope": "off", // Desactiva la necesidad de importar React
+      "react/jsx-uses-react": "off", // Desactiva la verificación de uso de React
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/display-name": "off",
+      "no-unsafe-optional-chaining": "warn"
     },
-  },
-)
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
+]);
